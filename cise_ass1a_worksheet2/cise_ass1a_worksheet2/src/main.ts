bdfd6671f5c1a665f59a045b2d43f8da7547d2ff
bombard21 <wangxiaofeng153@gmail.com>
2023-07-27 17:57:19 +1200

    YML

--- cise_ass1a_worksheet2/cise_ass1a_worksheet2/src/main.ts
+++ cise_ass1a_worksheet2/cise_ass1a_worksheet2/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotevn from 'dotenv';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  dotevn.config();
+  dotenv.config();
   const PORT = process.env.PORT || 5000;
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
