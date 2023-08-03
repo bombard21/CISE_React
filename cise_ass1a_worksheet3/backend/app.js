e715a939544768be0770b959d3f9d2d59d3cf1ff
bombard21 <wangxiaofeng153@gmail.com>
2023-08-03 22:33:41 +1200

    Made Backend

---
+++ cise_ass1a_worksheet3/backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/api/books', books)
+
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
