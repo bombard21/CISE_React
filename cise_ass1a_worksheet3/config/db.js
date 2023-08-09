1278d970732ba4a602559cea4a5ce4ae179846ec
bombard21 <wangxiaofeng153@gmail.com>
2023-08-09 12:40:44 +1200

    moved config to env

--- cise_ass1a_worksheet3/config/db.js
+++ cise_ass1a_worksheet3/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
 
