3b2ab71d8c2cc6bdacde2f11e81dff3fdd6edfe1
bombard21 <wangxiaofeng153@gmail.com>
2023-08-20 19:58:04 +1200

    yes

--- cise_ass1a_worksheet4/frontend/src/pages/_app.tsx
+++ cise_ass1a_worksheet4/frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../../styles/globals.css"
-import type { AppProps } from "next/app"
-import {SessionProvider} from "next-auth/react"
-import PopulatedNavBar from "../components/PopulatedNavBar"
+import "../../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import PopulatedNavBar from "../components/PopulatedNavBar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
